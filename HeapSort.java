12adb2fb492ba8bce669a3525d363462a9a6a2cb
allyasaf592 <115513872+allyasaf592@users.noreply.github.com>
2023-05-20 22:15:53 +0700

    Add files via upload

---
+++ HeapSort.java
@@ -0,0 +1,93 @@
+public class HeapSort {
+    public void sort(int arr[]) {
+        int N = arr.length;
+ 
+        // Build heap (rearrange array)
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+ 
+        // One by one extract an element from heap
+        for (int i = N - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+ 
+    void heapify(int arr[], int N, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);
+        }
+    }
+ 
+    static void printArray(int arr[]) {
+        int N = arr.length;
+ 
+        for (int i = 0; i < N; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Ascending Heap Sort
+    public static void ascendingHeapSort(int arr[]) {
+        int N = arr.length;
+ 
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+    }
+ 
+    // Descending Heap Sort
+    public static void descendingHeapSort(int arr[]) {
+        int N = arr.length;
+ 
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+ 
+        // Reverse the sorted array
+        for (int i = 0; i < N / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[N - i - 1];
+            arr[N - i - 1] = temp;
+        }
+    }
+ 
+    public static void main(String args[]) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int N = arr.length;
+ 
+        System.out.println("Array sebelum sorting: ");
+        printArray(arr);
+ 
+        // Ascending Heap Sort
+        ascendingHeapSort(arr);
+        System.out.println("Array setelah ascending heap sort: ");
+        printArray(arr);
+ 
+        // Descending Heap Sort
+        descendingHeapSort(arr);
+        System.out.println("Array setelah descending heap sort: ");
+        printArray(arr);
+    }
+}
